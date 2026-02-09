a2118807579641cc53d7832fc9adb4db890f74a0
rm-2278 <rm2278@cam.ac.uk>
2026-02-09 23:52:38 +0000

    Feb 9 commit 2

--- ActivationFunctions/main.py
+++ ActivationFunctions/main.py
@@ -5,6 +5,8 @@ import jax.numpy as jnp
 
 from flax import linen as nn
 
+import matplotlib.pyplot as plt
+
 
 DATASET_PATH = '../data'
 CHECKPOINT_PATH = '../checkpoints'
@@ -35,3 +37,24 @@ class Elu(nn.Module):
 class Swish(nn.Module):
     def __call__(self, x):
         return x * nn.sigmoid(x)
+    
+act_fn_dict = {
+    "sigmoid": Sigmoid,
+    "relu": Relu,
+    "leakyrelu": LeakyRelu,
+    "elu": Elu,
+    "swish": Swish
+}
+
+def get_grad(act_fn, x):
+    # grad requires act_fn to spit a scalar, so vmap batchifies it. 
+    #Circumvents the summation trick that is required otherwise.
+    return jax.vmap(jax.grad(act_fn))(x)
+
+def visulize_activation_function(act_fn):
+    
+
+act_fns = [act_fn() for act_fn in act_fn_dict]
+x = np.linspace(-5, 5, 1000)
+rows = np.ceil(len(act_fn_dict) / 2.)
+fig, ax = plt.
